7fa911a1e1a6c6c88da33b203c5cdc50bbcb0076
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2020-11-30 22:11:38 +0100

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const token = process.argv[2];
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const responses = require('./responses.json');
+
+
+client.on("message", msg => {
+    for (var a in responses.mathes) {
+        if (msg.content == a) msg.channel.send(responses.mathes[a]).catch(e => {
+            console.log(e);
+        });
+    }
+    for (a in responses.includes) {
+        if (msg.content.includes(a)) msg.channel.send(responses.mathes[a]).catch(e => {
+            console.log(e);
+        });
+    }
+    for (a in responses.startsWith) {
+        if (msg.content.startsWith(a)) msg.channel.send(responses.matmathes[a]).catch(e => {
+            console.log(e);
+        });
+    }
+    for (a in responses.endsWith) {
+        if (msg.content.endsWith(a)) msg.channel.send(responses.mathes[a]).catch(e => {
+            console.log(e);
+        });
+    }
+    for (a in responses.matchesUser) {
+        if (msg.author.username == a) msg.channel.send(responses.mathes[a]).catch(e => {
+            console.log(e);
+        });
+    }
+})
+
+
+client.login(token);
